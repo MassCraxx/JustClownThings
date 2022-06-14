dc8093327ac364e5def63ba05179febe310b726a
MassCraxx <masscraxx@users.noreply.github.com>
2022-06-15 00:45:48 +0200

    register items in hook, handle items in tick

--- Lua/Autorun/justclownthings.lua
+++ Lua/Autorun/justclownthings.lua
@@ -2,65 +2,51 @@
 -- by MassCraxx
 
 -- CONFIG
-local checkDelay = 3
-local checkTime = -1
+local DEBUG = true
+local ValidMoveHandling = true -- if this code would work as expected, this could prevent item movement from happening in the first place
+local CheckDelay = 3
+local CheckTime = -1
 
 local clownItems = {
     clowncostume = {
         InventorySlot = 3,
-        LimbSlot = InvSlotType.InnerClothes,
     },
     clownmask = {
         InventorySlot = 2,
-        LimbSlot = InvSlotType.Head,
     },
     clowndivingmask = {
         InventorySlot = 2,
-        LimbSlot = InvSlotType.Head,
     }
-
-    --TODO: add all diving suits
-    --TODO: sticky for all clown items
 }
 
-local forbiddenItems = {"divingsuit"}
+--TODO: add all diving suits
+local ForbiddenItems = {"divingsuit"}
+
+
+
+-- a list of all slots that can contain configured clown items
+local validSlots = {}
+for _, item in pairs(clownItems) do
+    validSlots[item.InventorySlot] = true
+end
 
 local clowns = {}
 local equippedClownItems = {}
+local dropItems = {}
 
 -- type: 6 = Server message, 7 = Console usage, 9 error
 Log = function (message)
-    Game.Log("[JustClownThings] " .. message, 6)
-end
-
-LimbToInventorySlot = function(limbSlot)
-    if limbSlot == InvSlotType.Head then
-        return 2
-    elseif limbSlot == InvSlotType.InnerClothes then
-        return 3
-    elseif limbSlot == InvSlotType.OuterClothes then
-        return 4
-    elseif limbSlot == InvSlotType.LeftHand then
-        return 5
-    elseif limbSlot == InvSlotType.RightHand then
-        return 6
+    if DEBUG then
+        Game.Log("[JustClownThings] " .. message, 6)
     end
 end
 
-WearsAnyItem = function (character, itemIds)
-    local head = character.Inventory.GetItemInLimbSlot(InvSlotType.Head)
-    local inner = character.Inventory.GetItemInLimbSlot(InvSlotType.InnerClothes)
-    local outer = character.Inventory.GetItemInLimbSlot(InvSlotType.OuterClothes)
-
-    for _, itemId in pairs(itemIds) do
-        if (head and head.Prefab.Identifier == itemId) or
-        (inner and inner.Prefab.Identifier == itemId) or
-        (outer and outer.Prefab.Identifier == itemId) then
-            return true
-        end
+FindClientCharacter = function (character)
+    for key, value in pairs(Client.ClientList) do
+        if character == value.Character then return value end
     end
 
-    return false
+    return nil
 end
 
 DropItem = function (item, character)
@@ -69,95 +55,86 @@ DropItem = function (item, character)
     item.Drop(character)
 end
 
---FindClientCharacter = function (character)
---    for key, value in pairs(Client.ClientList) do
---        if character == value.Character then return value end
---    end
---
---    return nil
---end
-
 Hook.Add("roundStart", "JustClownThings.RoundStart", function ()
-    --stickyItems = {}
     clowns = {}
     equippedClownItems = {}
+    dropItems = {}
 end)
-
-Hook.Add("item.drop", "JustClownThings.item.drop", function (item)
-    --print("drop" .. tostring(item))
-    --if equippedClownItems[item.ID] then
-    --    local character = equippedClownItems[item.ID]
-    --    -- clown has unequipped a clownitem -> put back on
-    --    print("unequipped")
-    --    --character.Inventory.TryPutItem(item, equippedClownItems[item.ID], {clownItem.LimbSlot}, true, true)
-    --    Timer.Wait(function ()
-    --        local clownItem = clownItems[tostring(item.Prefab.Identifier)]
-    --        local wornItem = character.Inventory.GetItemInLimbSlot(clownItem.LimbSlot)
-    --        -- if clown did not put on another clown item -> swap
-    --        if wornItem == nil or not clownItems[wornItem.Prefab.Identifier] then
-    --            character.Inventory.TryPutItem(item, equippedClownItems[item.ID], {clownItem.LimbSlot}, true, true)
-    --        end
-    --    end, 500)
-    --    return false
-    --end
-
-    return false
-end)
-
-Hook.Add("item.combine", "JustClownThings.item.drop", function (item, item2,user)
-    print("combine" .. tostring(item) .. tostring(item2))
-
-    return false
-end)
-
 
 Hook.Add("inventoryPutItem", "JustClownThings.inventoryPutItem", function (inventory, item, character, slot, removeItem)
     if character ~= nil and (character.IsBot or not character.IsHuman or character.IsDead) then return nil end
 
-    local clownItem = clownItems[tostring(item.Prefab.Identifier)]
+    local identifier = tostring(item.Prefab.Identifier)
+    local clownItem = clownItems[identifier]
+    print(clownItem)
     if clownItem then
-        print(tostring(equippedClownItems[item.ID]) .. " " .. slot)
-        print(inventory)
-        if character and (slot == 2 or slot == 3) then
-            -- player has equipped a clownitem
-            if not equippedClownItems[item.ID] and character.HasJob("assistant") then
-                -- assistant is clown now
-                if not clowns[character] then
-                    clowns[character] = {}
-                    print("player is clown")
-                end
-                
-                equippedClownItems[item.ID] = character
-                
-            else 
-                -- non-assistant -> drop
-                DropItem(item, character)
-                return false
+        if character and validSlots[slot] and inventory == character.Inventory then
+            -- clown item equipped
+            if character.IsAssistant then
+                equippedClownItems[slot] = {}
+                equippedClownItems[slot].Item = item 
+                equippedClownItems[slot].Character = character
+            else
+                -- non-assistants drop
+                dropItems[item] = character
             end
-        elseif equippedClownItems[item.ID] then
-            -- clown has unequipped a clownitem -> put back on
-            print("unequipped")
-            --character.Inventory.TryPutItem(item, equippedClownItems[item.ID], {clownItem.LimbSlot}, true, true)
-            Timer.Wait(function ()
-                local character = equippedClownItems[item.ID]
-                local wornItem = character.Inventory.GetItemInLimbSlot(clownItem.LimbSlot)
-                -- if clown did not put on another clown item -> swap
-                if character and (wornItem == nil or not clownItems[wornItem.Prefab.Identifier]) then
-                    character.Inventory.TryPutItem(item, equippedClownItems[item.ID], {clownItem.LimbSlot}, true, true)
-                end
-            end, 500)
-            print("false")
+        elseif ValidMoveHandling and equippedClownItems[clownItem.InventorySlot] and equippedClownItems[clownItem.InventorySlot].Item.ID == item.ID then
+            print("bla")
+            -- equipped item has been moved
+            --local character = equippedClownItems[clownItem.InventorySlot].Character
+            --print("inventoryPutItem: "..tostring(inventory) .. " item: " .. tostring(item) .. " character: ".. equippedClownItems[clownItem.InventorySlot].Character.Name .. " slot: " .. slot .. " remove: " .. tostring(removeItem))
+            if DEBUG then
+                character = equippedClownItems[clownItem.InventorySlot].Character
+                Log("Clown " .. character.Name .. " unequipped clown item " .. item.Name)
+            end
             return false
         end
     elseif clowns[character] then
-        -- clown has equipped forbiddenItem
-        for forbiddenItem in forbiddenItems do
-            if forbiddenItem == item.Prefab.Identifier then
-                DropItem(item, character)
-                return false
+        -- clowns drop forbiddenItems
+        for forbiddenItem in ForbiddenItems do
+            if identifier == forbiddenItem then
+                dropItems[item] = character
             end
         end
     end
 
     return nil
+end)
+
+Hook.Add("think", "JustClownThings.think", function ()
+    if Game.RoundStarted and #Client.ClientList > 0 then
+        if CheckTime and Timer.GetTime() > CheckTime then
+            CheckTime = Timer.GetTime() + CheckDelay
+
+            -- drop all items marked for drop
+            for item, character in pairs(dropItems) do
+                Log("Dropping forbidden item " .. item.Name .. " for" .. character.Name)
+                DropItem(item, character)
+            end
+            dropItems = {}
+
+            -- check for reequip
+            for slot, entry in pairs(equippedClownItems) do
+                local item = entry.Item
+                local character = entry.Character
+
+                if not item.Removed then
+                    -- if item is not equipped anymore
+                    if item.ParentInventory == nil or item.ParentInventory ~= character.Inventory or item.ParentInventory.FindIndex(item) ~= slot then
+                        character.Inventory.TryPutItem(item, slot, true, false, character, true, true)
+                        Log("Reequipping clown item to clown "..character.Name)
+                    elseif not clowns[character] then
+                        -- if its the first item ever equipped inform user
+                        clowns[character] = true
+
+                        local client = FindClientCharacter(character)
+                        if client then
+                            Game.SendDirectChatMessage("", "PRAISE THE HONKMOTHER!", nil, ChatMessageType.Error, client)
+                        end
+                        Log(character.Name .. " equipped a clown item " .. item.Name .. " and is now a clown.")
+                    end
+                end
+            end
+        end
+    end
 end)
