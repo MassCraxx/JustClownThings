ba7f460cc7dcb182a42374a2609e97fa6c84dd6c
MassCraxx <masscraxx@users.noreply.github.com>
2022-06-15 04:41:46 +0200

    handle multiple clowns, handle CancelFabricator, add all diving suits, move forbidden handling to tick

--- Lua/Autorun/justclownthings.lua
+++ Lua/Autorun/justclownthings.lua
@@ -3,7 +3,6 @@
 
 -- CONFIG
 local DEBUG = true
-local ValidMoveHandling = true -- if this code would work as expected, this could prevent item movement from happening in the first place
 local CheckDelay = 3
 local CheckTime = -1
 
@@ -19,9 +18,15 @@ local clownItems = {
     }
 }
 
---TODO: add all diving suits
-local ForbiddenItems = {"divingsuit"}
+-- these items will be dropped as clown. Key is InventorySlot index, value is the item id
+local ForbiddenItems = {}
+ForbiddenItems[4] = {"divingsuit","combatdivingsuit","abyssdivingsuit","slipsuit","pucs"}
 
+-- if true, will drop items that are moved to active fabricator to the ground before reequipping them to stop the craft
+local CancelFabricator = true
+
+-- EXPERIMENTAL: this takes more performance, but could potentially prevent attached items from moving in the first place
+local ValidMoveHandling = false 
 
 
 -- a list of all slots that can contain configured clown items
@@ -64,36 +69,32 @@ end)
 Hook.Add("inventoryPutItem", "JustClownThings.inventoryPutItem", function (inventory, item, character, slot, removeItem)
     if character ~= nil and (character.IsBot or not character.IsHuman or character.IsDead) then return nil end
 
-    local identifier = tostring(item.Prefab.Identifier)
-    local clownItem = clownItems[identifier]
-    print(clownItem)
+    local clownItem = clownItems[item.Prefab.Identifier.Value]
     if clownItem then
         if character and validSlots[slot] and inventory == character.Inventory then
             -- clown item equipped
             if character.IsAssistant then
-                equippedClownItems[slot] = {}
-                equippedClownItems[slot].Item = item 
-                equippedClownItems[slot].Character = character
+                if not equippedClownItems[character] then 
+                    equippedClownItems[character] = {}
+                end
+                equippedClownItems[character][slot] = item 
             else
                 -- non-assistants drop
                 dropItems[item] = character
             end
-        elseif ValidMoveHandling and equippedClownItems[clownItem.InventorySlot] and equippedClownItems[clownItem.InventorySlot].Item.ID == item.ID then
-            print("bla")
-            -- equipped item has been moved
-            --local character = equippedClownItems[clownItem.InventorySlot].Character
-            --print("inventoryPutItem: "..tostring(inventory) .. " item: " .. tostring(item) .. " character: ".. equippedClownItems[clownItem.InventorySlot].Character.Name .. " slot: " .. slot .. " remove: " .. tostring(removeItem))
-            if DEBUG then
-                character = equippedClownItems[clownItem.InventorySlot].Character
-                Log("Clown " .. character.Name .. " unequipped clown item " .. item.Name)
-            end
-            return false
-        end
-    elseif clowns[character] then
-        -- clowns drop forbiddenItems
-        for forbiddenItem in ForbiddenItems do
-            if identifier == forbiddenItem then
-                dropItems[item] = character
+        elseif ValidMoveHandling then 
+            for user, entry in pairs(equippedClownItems) do
+                for oldSlot, equippedItem in pairs(entry) do
+                    if equippedItem.ID == item.ID then
+                        if DEBUG then
+                            character = user
+                            Log("Clown " .. character.Name .. " unequipped clown item " .. item.Name)
+                            --print("inventoryPutItem: "..tostring(inventory) .. " item: " .. tostring(item) .. " character: ".. character.Name .. " slot: " .. slot .. " remove: " .. tostring(removeItem))
+                        end
+                        return false
+                    end
+                end
+                
             end
         end
     end
@@ -106,35 +107,51 @@ Hook.Add("think", "JustClownThings.think", function ()
         if CheckTime and Timer.GetTime() > CheckTime then
             CheckTime = Timer.GetTime() + CheckDelay
 
+            -- check clowns for reequip
+            for character, entry in pairs(equippedClownItems) do
+                for slot, item in pairs(entry) do
+                    if not item.Removed then
+                        if item.ParentInventory == nil or item.ParentInventory ~= character.Inventory or item.ParentInventory.FindIndex(item) ~= slot then
+                            -- if item is not equipped anymore
+                            Log("Reequipping clown item to clown "..character.Name)
+                            if CancelFabricator and item.ParentInventory and item.ParentInventory.Locked then
+                                -- if item is in an active/locked fabricator, drop it before re-equip to stop the craft
+                                dropItems[item] = character
+                            else
+                                -- otherwise reequip
+                                character.Inventory.TryPutItem(item, slot, true, false, character, true, true)
+                            end
+                        elseif not clowns[character] then
+                            -- if its the first clown item equipped inform user
+                            clowns[character] = true
+
+                            local client = FindClientCharacter(character)
+                            if client then
+                                Game.SendDirectChatMessage("", "PRAISE THE HONKMOTHER!", nil, ChatMessageType.Error, client)
+                            end
+                            Log(character.Name .. " equipped a clown item " .. item.Name .. " and is now a clown.")
+                        end
+                    end
+                end
+
+                -- check forbidden items for this clown
+                for slot, identifiers in pairs(ForbiddenItems) do
+                    local wornItem = character.Inventory.GetItemAt(slot)
+                    for identifier in identifiers do
+                        if wornItem and wornItem.Prefab.Identifier == identifier then
+                            dropItems[wornItem] = character
+                            break
+                        end
+                    end
+                end
+            end
+        
             -- drop all items marked for drop
             for item, character in pairs(dropItems) do
                 Log("Dropping forbidden item " .. item.Name .. " for" .. character.Name)
                 DropItem(item, character)
             end
             dropItems = {}
-
-            -- check for reequip
-            for slot, entry in pairs(equippedClownItems) do
-                local item = entry.Item
-                local character = entry.Character
-
-                if not item.Removed then
-                    -- if item is not equipped anymore
-                    if item.ParentInventory == nil or item.ParentInventory ~= character.Inventory or item.ParentInventory.FindIndex(item) ~= slot then
-                        character.Inventory.TryPutItem(item, slot, true, false, character, true, true)
-                        Log("Reequipping clown item to clown "..character.Name)
-                    elseif not clowns[character] then
-                        -- if its the first item ever equipped inform user
-                        clowns[character] = true
-
-                        local client = FindClientCharacter(character)
-                        if client then
-                            Game.SendDirectChatMessage("", "PRAISE THE HONKMOTHER!", nil, ChatMessageType.Error, client)
-                        end
-                        Log(character.Name .. " equipped a clown item " .. item.Name .. " and is now a clown.")
-                    end
-                end
-            end
         end
     end
 end)
